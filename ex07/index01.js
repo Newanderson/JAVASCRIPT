b524723aefb3387b9db43aa201e9d6ce56357f5d
Newanderson Souza <108943716+Newanderson@users.noreply.github.com>
2022-09-02 12:21:22 -0300

    if e else

---
+++ ex07/index01.js
@@ -0,0 +1,7 @@
+var vel = 63
+console.log(`a velocidade do veiculos e ${vel}`) 
+if(vel >60){
+    console.log(`voce ultrapassou a velocidade permitida`)
+}
+
+console.log(`diriga sempre usasndo cinto de seguraca`)
