70f80ed64034fc8abdc2652e2976a9c807c7abbe
Newanderson Souza <108943716+Newanderson@users.noreply.github.com>
2022-10-19 11:02:36 -0300

    hora do dia

--- ex012.html/modelo/script.js
+++ ex012.html/modelo/script.js
@@ -1,9 +1,10 @@
-@ -1,27 +0,0 @@
+
 function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    
    
      msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
@@ -14,7 +15,7 @@ function carregar() {
         
     }else if (hora >= 12 && hora < 18) {
 
-            img.src = 'imagens/tarde.png'
+            img.src = 'imagens/trade.png'
             document.body.style.background = '#F2622E'
     }else {
             
